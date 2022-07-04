527483e7eeddfb5848615c096c46cdbc57a0c81b
carloshzoghbi <37075619+carloshzoghbi@users.noreply.github.com>
2022-07-04 18:41:11 -0500

    Update delete-all.sh

--- delete-all.sh
+++ delete-all.sh
@@ -7,14 +7,35 @@ kubectl delete -f customresourcedefinitions.yaml
 kubectl delete -f ingresslink-customresourcedefinition.yaml
 
 #Remove nginx resources.
-kubectl delete -f kubernetes-ingress/deployments/cafe-ingress.yaml
-kubectl delete -f kubernetes-ingress/deployments/cafe-secret.yaml
-kubectl delete -f kubernetes-ingress/deployments/cafe.yaml
-
-kubectl delete namespace nginx-ingress
-kubectl delete clusterrole nginx-ingress
-kubectl delete clusterrolebinding nginx-ingress
-kubectl delete -f kubernetes-ingress/deployments/common/crds
+kubectl delete -f common/ns-and-sa.yaml
+kubectl delete -f deployment/appprotect-dos-arb.yaml
+kubectl delete -f service/appprotect-dos-arb-svc.yaml
+kubectl delete -f rbac/rbac.yaml
+kubectl delete -f rbac/ap-rbac.yaml
+kubectl delete -f rbac/apdos-rbac.yaml
+kubectl delete -f common/default-server-secret.yaml
+kubectl delete -f nginx-config.yaml
+kubectl delete -f common/ingress-class.yaml
+kubectl delete -f common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl delete -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl delete -f common/crds/k8s.nginx.org_transportservers.yaml
+kubectl delete -f common/crds/k8s.nginx.org_policies.yaml
+kubectl delete -f common/crds/k8s.nginx.org_globalconfigurations.yaml
+kubectl delete -f common/crds/appprotect.f5.com_aplogconfs.yaml
+kubectl delete -f common/crds/appprotect.f5.com_appolicies.yaml
+kubectl delete -f common/crds/appprotect.f5.com_apusersigs.yaml
+kubectl delete -f common/crds/appprotectdos.f5.com_apdoslogconfs.yaml
+kubectl delete -f common/crds/appprotectdos.f5.com_apdospolicy.yaml
+kubectl delete -f common/crds/appprotectdos.f5.com_dosprotectedresources.yaml
+kubectl delete -f daemon-set/nginx-plus-ingress.yaml
+kubectl delete -f loadbalancer.yaml
+kubectl delete -f cafe.yaml
+kubectl delete -f cafe-secret.yaml
+kubectl delete -f ap-logconf.yaml
+kubectl delete -f ap-dataguard-alarm-policy.yaml
+kubectl delete -f ap-apple-uds.yaml
+kubectl delete -f cafe-ingress.yaml
+kubectl delete -f nodeport.yaml
 
 #Remove big-ip cis resources. Warning: This will delete all BIG-IP CIS resources created in the parent directory as well.
 kubectl delete -f https://raw.githubusercontent.com/laul7klau/kubernetes-aws/main/bigip-ctrl-ingress/config/k8s-rbac.yaml
